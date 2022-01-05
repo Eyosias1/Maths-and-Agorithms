de02d8eb654f5290ad3053556f1e1e743392dedc
Eyosias1 <eyosias16@gmail.com>
2022-01-05 18:15:59 +0100

    Create LeTest.h
    test header file

---
+++ Gauss-and-Jacobi-Method/code_source/header/LeTest.h
@@ -0,0 +1,14 @@
+#ifndef LETEST_H
+#define LETEST_H
+
+void   ABordCaree(float **A,int size);
+void    DingDong(float ** A, int size);
+void    Franc(float ** A, int size);
+void    Hilber(float ** A, int size);
+void    Hilber2(float ** A, int size);
+void    Kms(float ** A, int size);
+void    Lehmar(float ** A, int size);
+void    Lotkin(float ** A, int size);
+void    Moler(float ** A, int size);
+
+#endif
